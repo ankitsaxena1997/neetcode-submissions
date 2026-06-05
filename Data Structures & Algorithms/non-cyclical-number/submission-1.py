46c654033bf20d6dfce1038e30bf671a712cca9d
Ankit Saxena <saxenaankitkumar5@gmail.com>
2026-06-05 15:23:16 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def isHappy(self, n: int) -> bool:
+
+        seen= set()  
+        
+        while n!=1 :
+
+            total=0
+            while n!=0 :
+                total+= (n%10)**2
+                n=n//10
+            
+            n=total
+
+            if total in seen:
+                return False
+            else:
+                seen.add(total)
+        
+        return True
+
+
+        
+
+        
+        
